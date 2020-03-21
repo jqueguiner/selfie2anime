2f1884d2fed67635daa1ac09d581d4691dad1c63
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2020-03-21 14:50:14 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -251,7 +251,7 @@ if __name__ == '__main__':
     create_directory(model_directory)
 
 
-    url_prefix = 'http://pretrained-models.auth-18b62333a540498882ff446ab602528b.storage.gra5.cloud.ovh.net/image/'
+    url_prefix = 'http://pretrained-models.auth-18b62333a540498882ff446ab602528b.storage.gra.cloud.ovh.net/image/'
 
     model_file_rar = 'UGATIT_selfie2anime_lsgan_4resblock_6dis_1_1_10_10_1000_sn_smoothing.rar'
 
